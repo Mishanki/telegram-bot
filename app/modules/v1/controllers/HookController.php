5458f9321cbc816373d496aff64e7793c5dc8b37
Mikhail <m.utkin@zaorotec.ru>
2021-10-18 15:57:51 +0300

    Unset hook

--- app/modules/v1/controllers/HookController.php
+++ app/modules/v1/controllers/HookController.php
@@ -4,6 +4,7 @@ namespace app\modules\v1\controllers;
 
 use app\modules\v1\actions\hook\HookManagerAction;
 use app\modules\v1\actions\hook\SetHookAction;
+use app\modules\v1\actions\hook\UnsetHookAction;
 use app\modules\v1\core\ApiController;
 
 class HookController extends ApiController
@@ -13,6 +14,9 @@ class HookController extends ApiController
         return [
             'set' => [
                 'class' => SetHookAction::class,
+            ],
+            'unset' => [
+                'class' => UnsetHookAction::class,
             ],
             'manager' => [
                 'class' => HookManagerAction::class
