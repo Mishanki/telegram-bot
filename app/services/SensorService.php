08f60f09aff96e18317cfdf707b8d584a154dfba
Mikhail <m.utkin@zaorotec.ru>
2021-10-21 12:02:23 +0300

    Обновил месседж

--- app/services/SensorService.php
+++ app/services/SensorService.php
@@ -64,7 +64,7 @@ class SensorService
     private function formatter(array $data): string
     {
         $type = 'µg/m³';
-        $msg[0] = 'Данные PM 2.5 за последние 5 мин:';
+        $msg[0] = 'Среднее pm 2.5 за последние 5 минут:';
 
         foreach ($data['data'] as $senId => $items) {
             foreach ($items as $time => $v) {
