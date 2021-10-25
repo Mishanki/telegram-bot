e305573b4b8004ff17a6acf4f1e038a6834ae9fb
Mikhail <m.utkin@zaorotec.ru>
2021-10-25 12:26:23 +0300

    Добавил alert

--- app/models/dictionary/ThresholdDictionary.php
+++ app/models/dictionary/ThresholdDictionary.php
@@ -4,7 +4,7 @@ namespace app\models\dictionary;
 
 class ThresholdDictionary
 {
-    const PM_25_UPPER_THRESHOLD_24 = 2; // среднесуточная
+    const PM_25_UPPER_THRESHOLD_24 = 15; // среднесуточная
     const PM_25_UPPER_THRESHOLD_ANNUAL = 5; // среднегодовая
 
     const PM_10_UPPER_THRESHOLD_24 = 45; // среднесуточная
