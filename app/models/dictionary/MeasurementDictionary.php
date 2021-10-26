85d61ec80b833cb96f344e06eaab727ffe5471ff
Mikhail <m.utkin@zaorotec.ru>
2021-10-26 15:24:20 +0300

    refactoring

--- app/models/dictionary/MeasurementDictionary.php
+++ app/models/dictionary/MeasurementDictionary.php
@@ -5,5 +5,5 @@ namespace app\models\dictionary;
 class MeasurementDictionary
 {
     const MEASUREMENT_PM_RU = 'мкг/м³';
-    const MEASUREMENT_PM_EN = '';
+    const MEASUREMENT_PM_EN = 'μg/m³';
 }
--- app/models/dictionary/ThresholdDictionary.php
+++ app/models/dictionary/ThresholdDictionary.php
@@ -4,9 +4,9 @@ namespace app\models\dictionary;
 
 class ThresholdDictionary
 {
-    const PM_25_UPPER_THRESHOLD_24 = 15; // среднесуточная
-    const PM_25_UPPER_THRESHOLD_ANNUAL = 5; // среднегодовая
+    const PM_25_UPPER_THRESHOLD_24 = 25; // среднесуточная
+    const PM_25_UPPER_THRESHOLD_ANNUAL = 10; // среднегодовая
 
-    const PM_10_UPPER_THRESHOLD_24 = 45; // среднесуточная
-    const PM_10_UPPER_THRESHOLD_ANNUAL = 15; // среднегодовая
+    const PM_10_UPPER_THRESHOLD_24 = 50; // среднесуточная
+    const PM_10_UPPER_THRESHOLD_ANNUAL = 20; // среднегодовая
 }
