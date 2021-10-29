e249f9b57f6ff5cb861092e1638d3c5c96a7dced
Mikhail Utkin <ufvbkmnjy>
2021-10-29 21:59:47 +0300

    debug

--- app/modules/v1/actions/manager/HookManagerAction.php
+++ app/modules/v1/actions/manager/HookManagerAction.php
@@ -5,6 +5,7 @@ namespace app\modules\v1\actions\manager;
 use app\core\Bot;
 use app\modules\v1\core\Action;
 use app\services\ManagerService;
+use app\services\MessageService;
 
 class HookManagerAction extends Action
 {
@@ -18,6 +19,14 @@ class HookManagerAction extends Action
 
     public function run(): bool
     {
+        $msg = file_get_contents('php://input');
+        $msg = json_decode($msg, true);
+        $msg = json_encode($msg, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+
+        /* @var $message MessageService */
+        $message = Bot::$container->get(MessageService::class);
+        $message->sendMessageByChatId($msg);
+
         $this->manager->hook();
 
         return true;
